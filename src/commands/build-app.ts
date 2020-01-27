e5227bc71dedb23531c0ef9562eebe247dd6f9e3
Ole-Richard Blindheim <oblin@equinor.com>
2020-01-27 09:22:03 +0100

    Build Image patch fix

--- src/commands/build-app.ts
+++ src/commands/build-app.ts
@@ -302,7 +302,7 @@ export default class BuildApp extends Command {
             babel,
             mode(true),
             entry(context.package, true),
-            images(`/apps/${context.manifest.key}/resources/`),
+            images(`/bundles/apps/${context.manifest.key}/resources/`),
             externals(cliDependencies, moduleDependencies),
             styles,
             typescript(context.appOutputDir, true),
