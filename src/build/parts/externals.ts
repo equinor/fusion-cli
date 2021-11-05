e18bd6dacdfc9d55339e4c271651fc5b95567345
odin thomas rochmann <odin.rochmann@gmail.com>
2021-11-05 09:04:38 +0100

    fix: make react externals in builds

--- src/build/parts/externals.ts
+++ src/build/parts/externals.ts
@@ -6,40 +6,41 @@ import IDependencyList from '../DependencyList';
  * @param version version string
  */
 const parseVersion = (version?: string | null) =>
-    version
-        ? version
-              .split('.')
-              .map(part => part.replace(/[^0-9]+/, ''))
-              .join('.')
-        : '';
+  version
+    ? version
+        .split('.')
+        .map((part) => part.replace(/[^0-9]+/, ''))
+        .join('.')
+    : '';
 
 export default (cliDependencies: IDependencyList, moduleDependencies: IDependencyList): webpack.Configuration => {
+  const externals: Record<string, string> = {};
+  if (!process.env.DEVELOPMENT) {
+    externals['@equinor/fusion-react-styles'] = 'FusionReactStyles';
+  }
 
-    const externals: Record<string, string> = {};
-    if(!process.env.DEVELOPMENT) {
-        externals['@equinor/fusion-react-styles'] = 'FusionReactStyles';
+  const addIfVersionsMatch = (key: string, value: string) => {
+    const moduleVersion = parseVersion(moduleDependencies[key]);
+    const cliVersion = parseVersion(cliDependencies[key]);
+
+    if (!moduleVersion || cliVersion === moduleVersion) {
+      externals[key] = value;
+    } else {
+      console.warn(
+        `Using ${key}@${moduleVersion} instead of the built in version (v${cliVersion})! This will add to the bundle size!`
+      );
     }
+  };
 
-    const addIfVersionsMatch = (key: string, value: string) => {
-        const moduleVersion = parseVersion(moduleDependencies[key]);
-        const cliVersion = parseVersion(cliDependencies[key]);
-
-        if (!moduleVersion || cliVersion === moduleVersion) {
-            externals[key] = value;
-        } else {
-            console.warn(
-                `Using ${key}@${moduleVersion} instead of the built in version (v${cliVersion})! This will add to the bundle size!`
-            );
-        }
-    };
+  // enforce externals on React libraries
+  externals['react'] = 'FusionReact';
+  externals['react-dom'] = 'FusionReactDOM';
+  externals['react-router-dom'] = 'FusionReactRouterDOM';
 
     addIfVersionsMatch('@equinor/fusion', 'FusionAPI');
     addIfVersionsMatch('@equinor/fusion-components', 'FusionComponents');
-    addIfVersionsMatch('react', 'FusionReact');
-    addIfVersionsMatch('react-dom', 'FusionReactDOM');
-    addIfVersionsMatch('react-router-dom', 'FusionReactRouterDOM');
 
-    return {
-        externals,
-    };
+  return {
+    externals,
+  };
 };
