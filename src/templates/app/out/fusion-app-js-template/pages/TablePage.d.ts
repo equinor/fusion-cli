cb83af261d40bf886b6e791d5db27262134b2ca6
Andrej Nikolic <annik@bouvet.no>
2022-05-11 08:39:16 +0200

    small fixes

--- src/templates/app/out/fusion-app-js-template/pages/TablePage.d.ts
+++ src/templates/app/out/fusion-app-js-template/pages/TablePage.d.ts
@@ -1,8 +1,6 @@
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 /**
  *
- * @returns AG grid table page for displaying fusion apps
+ * @returns AG Grid Table page for displaying fusion apps
  */
 export declare const TablePage: () => JSX.Element;
 export default TablePage;
