aaf4425cc7f6572d7deb7ea1421d735bf34ccff2
Andrej Nikolic <annik@bouvet.no>
2022-05-11 09:51:35 +0200

    fix: dependencies

--- src/templates/app/out/fusion-app-js-template/App.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- *
- * @returns Add React Router and ReactDev tool
- * React Router @see (@link https://reactrouterdotcom.fly.dev/docs/en/v6/getting-started/installation)
- * React Query @see (@link https://react-query.tanstack.com/quick-start)
- */
-export declare const AppComponent: () => JSX.Element;
-export default AppComponent;
--- src/templates/app/out/fusion-app-js-template/pages/UserPage.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * User page
- *
- * @returns Users personal details
- */
-export declare const UserPage: () => JSX.Element;
-export default UserPage;
