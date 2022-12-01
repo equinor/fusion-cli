29af95c763f5e096b7429a5ff823e1e162437419
Øyvind Eikeland <oyvind@eikeland.me>
2022-12-01 14:54:21 +0100

    fix(report): optimize report template without context

--- src/scripts/update-template-files.ts
+++ src/scripts/update-template-files.ts
@@ -4,8 +4,10 @@ import templateDirs from './template-dirs';
 
 const root = resolve(process.cwd(), 'src', 'templates');
 const dir = join(root, '.config');
-const files = readdirSync(dir).map(path => join(dir, path)).filter(path => statSync(path).isFile());
+const files = readdirSync(dir)
+  .map((path) => join(dir, path))
+  .filter((path) => statSync(path).isFile());
 
-const copyFiles = (dir: string) => files.forEach(file => copyFileSync(file, join(dir, basename(file))));
+const copyFiles = (dir: string) => files.forEach((file) => copyFileSync(file, join(dir, basename(file))));
 
 templateDirs(root).forEach(copyFiles);
--- src/templates/report/src/LandingPage/index.tsx
+++ src/templates/report/src/LandingPage/index.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+/** @ts-ignore */
 import * as styles from './styles.less';
 
 const LandingPage = (): JSX.Element => {
