ae9411b22ece611c253afbbbddb4da5dbd901263
Øyvind Eikeland <oyvind@eikeland.me>
2023-01-03 16:21:52 +0100

    fix(context): router example routes with new framework

--- src/templates/app/src/Router.tsx
+++ src/templates/app/src/Router.tsx
@@ -1,6 +1,6 @@
-import { useMemo } from 'react';
-
-import { useRoutes, RouteObject, matchPath, useLocation } from 'react-router-dom';
+import { useHistory } from '@equinor/fusion';
+import { useState, useCallback, useEffect, useMemo } from 'react';
+import { useNavigate, useRoutes, RouteObject, matchPath, useLocation } from 'react-router-dom';
 
 import Icon from '@equinor/fusion-react-icon';
 
@@ -22,21 +22,46 @@ export const pages: Pages[] = [
   { name: 'Context', path: 'context', element: <ContextPage />, icon: <Icon icon="donut_outlined" /> },
 ];
 
-export const useCurrentPage = () => {
+export const useCurrentPage = (): Pages | undefined => {
   const { pathname } = useLocation();
   return useMemo(() => pages.find((page) => !!matchPath({ path: page.path ?? '/' }, pathname)), [pathname]);
+};
+
+export const useNavHack = (): {
+  isPortal: boolean;
+  isAppPath: (pathname: string) => boolean | RegExpMatchArray | null;
+  basename: string;
+} => {
+  /** since the portal has done routing before the app renders, check the path */
+  const [isPortal] = useState(!!window.location.pathname.match(/^\/?apps/));
+  const [basename] = useState(isPortal ? '/apps/test-app' : '/');
+  const isAppPath = useCallback(
+    (pathname: string) => !isPortal || (isPortal && pathname.match(basename)),
+    [isPortal, basename]
+  );
+  return { isPortal, isAppPath, basename };
 };
 
 /**
  *
  * @returns Routes and Route for rendering different pages in React Router
  */
-export const Router = () => {
-  /**
-   * Routes is the parent that looks through all its children so it can find and render the best match for Route.
-   * Route renders its element if there is a match with the Route and its path
-   */
-
+export const Router = (): JSX.Element | null => {
+  const fusionHistory = useHistory();
+  const navigate = useNavigate();
+  const { basename, isAppPath } = useNavHack();
+  useEffect(
+    () =>
+      fusionHistory.listen((e) => {
+        /** hack since app is unloaded after navigation outside app */
+        if (isAppPath(e.pathname)) {
+          /** hack to remove app prefix from path */
+          e.pathname = e.pathname.replace(basename, '');
+          navigate(e);
+        }
+      }),
+    [fusionHistory, navigate, isAppPath, basename]
+  );
   return useRoutes([{ path: '/:contextId/', element: <Layout />, children: pages }]);
 };
 
