ea0a264fbd723ae27ab9ef1d5dd58bf59a408660
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2023-01-05 14:12:18 +0100

    feat: use custom context and app container

--- src/start-app/Portal.tsx
+++ src/start-app/Portal.tsx
@@ -1,8 +1,7 @@
 /* eslint-disable react/no-multi-comp */
 import { FunctionComponent, useRef } from 'react';
-import { Router } from 'react-router';
 
-import { createFusionContext, FusionContext, ServiceResolver } from '@equinor/fusion';
+import { FusionContext } from '@equinor/fusion';
 import { ThemeProvider } from '@equinor/fusion-react-styles';
 import {
   FusionRoot,
@@ -12,31 +11,16 @@ import {
   ContextSelector,
 } from '@equinor/fusion-components';
 
+import { createFusionContext } from './create-fusion-context';
+
 import { HotAppWrapper } from './HotAppWrapper';
-import createAuthContainer from './create-auth-container';
 import { BrowserRouter } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+
+import { useFramework } from '@equinor/fusion-framework-react';
+import { AppModule } from '@equinor/fusion-framework-module-app';
 
 const HeaderContextSelector: FunctionComponent<HeaderContentProps> = ({ app }) => {
   return app?.context?.types.length ? <ContextSelector /> : null;
-};
-
-const serviceResolver: ServiceResolver = {
-  getContextBaseUrl: () => 'https://pro-s-context-ci.azurewebsites.net',
-  getDataProxyBaseUrl: () => 'https://pro-s-dataproxy-ci.azurewebsites.net',
-  getFusionBaseUrl: () => 'https://pro-s-portal-ci.azurewebsites.net',
-  getMeetingsBaseUrl: () => 'https://pro-s-meeting-v2-ci.azurewebsites.net',
-  getOrgBaseUrl: () => 'https://pro-s-org-ci.azurewebsites.net',
-  getPowerBiBaseUrl: () => 'https://pro-s-powerbi-ci.azurewebsites.net',
-  getProjectsBaseUrl: () => 'https://pro-s-projects-ci.azurewebsites.net',
-  getTasksBaseUrl: () => 'https://pro-s-tasks-ci.azurewebsites.net',
-  getPeopleBaseUrl: () => 'https://pro-s-people-ci.azurewebsites.net',
-  getReportsBaseUrl: () => 'https://pro-s-reports-ci.azurewebsites.net',
-  getPowerBiApiBaseUrl: () => 'https://api.powerbi.com/v1.0/myorg',
-  getNotificationBaseUrl: () => 'https://pro-s-notification-ci.azurewebsites.net',
-  getInfoUrl: () => 'https://pro-s-info-app-ci.azurewebsites.net',
-  getFusionTasksBaseUrl: () => 'https://pro-s-tasks-ci.azurewebsites.net',
-  getBookmarksBaseUrl: () => 'https://pro-s-bookmarks-ci.azurewebsites.net',
 };
 
 export const Portal = () => {
@@ -49,38 +33,27 @@ export const Portal = () => {
   const root = useRef(rootEl);
   const overlay = useRef(overlayEl);
 
-  // replace with hook
-  const authContainer = createAuthContainer(); //({ framework: window.Fusion });
-
   const headerContent = useRef<HTMLElement | null>(null);
   const headerAppAside = useRef<HTMLElement | null>(null);
 
-  const browserHistory = createBrowserHistory();
-  const fusionContext = createFusionContext(
-    authContainer,
-    serviceResolver,
-    {
+  const framework = useFramework<[AppModule]>();
+
+  const fusionContext = createFusionContext({
+    framework,
+    refs: {
       headerAppAside,
       headerContent,
       overlay,
       root,
     },
-    {
-      environment: {
-        env: 'dev',
-      },
-      loadBundlesFromDisk: false,
-    },
-    //@ts-ignore
-    browserHistory
-  );
+  });
 
   return (
     <FusionContext.Provider value={fusionContext}>
       <ThemeProvider seed="fusion-dev-app">
         <FusionRoot rootRef={root} overlayRef={overlay}>
           <BrowserRouter>
-              <FusionHeader aside={null} content={HeaderContextSelector} start={null} settings={null} />
+            <FusionHeader aside={null} content={HeaderContextSelector} start={null} settings={null} />
           </BrowserRouter>
           <FusionContent>
             <HotAppWrapper />
