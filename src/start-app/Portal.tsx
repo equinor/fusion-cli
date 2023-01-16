8659dee6d27590f54684ea95960ba904bdb45888
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2023-01-16 12:16:26 +0100

    feat: use package from fusion framework for interopt

--- src/start-app/Portal.tsx
+++ src/start-app/Portal.tsx
@@ -1,58 +1,34 @@
 /* eslint-disable react/no-multi-comp */
-import React, { useRef } from 'react';
+import React from 'react';
 
-import { FusionContext } from '@equinor/fusion';
-import { ThemeProvider, theme } from '@equinor/fusion-react-styles';
-import { FusionRoot, FusionHeader, FusionContent } from '@equinor/fusion-components';
-
-import { createFusionContext } from './create-fusion-context';
-
-import { HotAppWrapper } from './HotAppWrapper';
 import { BrowserRouter } from 'react-router-dom';
 
 import { useFramework } from '@equinor/fusion-framework-react';
-import { AppModule } from '@equinor/fusion-framework-module-app';
+import type { AppModule } from '@equinor/fusion-framework-module-app';
+import type { NavigationModule } from '@equinor/fusion-framework-module-navigation';
+import { LegacyFusionWrapper } from '@equinor/fusion-framework-legacy-interopt/components';
+
+import { ThemeProvider, theme } from '@equinor/fusion-react-styles';
+import { FusionHeader, FusionContent } from '@equinor/fusion-components';
+import { StarProgress } from '@equinor/fusion-react-progress-indicator';
 
 import { ContextSelector } from './components/ContextSelector';
-import { NavigationModule } from '@equinor/fusion-framework-module-navigation';
+
+import { HotAppWrapper } from './HotAppWrapper';
 
 export const Portal = () => {
-  const rootEl = document.createElement('div');
-  const overlayEl = document.createElement('div');
-
-  rootEl.appendChild(overlayEl);
-
-  const root = useRef(rootEl);
-  const overlay = useRef(overlayEl);
-
-  const headerContent = useRef<HTMLElement | null>(null);
-  const headerAppAside = useRef<HTMLElement | null>(null);
-
   const framework = useFramework<[AppModule, NavigationModule]>();
-
-  const fusionContext = createFusionContext({
-    framework,
-    refs: {
-      headerAppAside,
-      headerContent,
-      overlay,
-      root,
-    },
-  });
-
   return (
-    <FusionContext.Provider value={fusionContext}>
-      <ThemeProvider theme={theme}>
-        <FusionRoot rootRef={root} overlayRef={overlay}>
-          <BrowserRouter>
-            <FusionHeader aside={null} content={ContextSelector} start={null} settings={null} />
-          </BrowserRouter>
-          <FusionContent>
-            <HotAppWrapper />
-          </FusionContent>
-        </FusionRoot>
-      </ThemeProvider>
-    </FusionContext.Provider>
+    <ThemeProvider theme={theme}>
+      <LegacyFusionWrapper framework={framework} loader={<StarProgress />} options={{ loadBundlesFromDisk: true }}>
+        <BrowserRouter>
+          <FusionHeader aside={null} content={ContextSelector} start={null} settings={null} />
+        </BrowserRouter>
+        <FusionContent>
+          <HotAppWrapper />
+        </FusionContent>
+      </LegacyFusionWrapper>
+    </ThemeProvider>
   );
 };
 
