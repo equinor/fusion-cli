3ae1c8c96ca7256e1a716f5e86bab28e04c66cdf
Øyvind Eikeland <oyvind@eikeland.me>
2023-03-14 16:07:33 +0100

    chore: updating to framework beta version

--- src/start-app/components/ContextSelector/ContextSelector.tsx
+++ src/start-app/components/ContextSelector/ContextSelector.tsx
@@ -1,27 +1,39 @@
-import React, { useEffect, useMemo, useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useMemo } from 'react';
 import {
   ContextProvider,
-  ContextResultItem,
   ContextSearch,
   ContextSearchProps,
   ContextSelectEvent,
 } from '@equinor/fusion-react-context-selector';
+import { useFramework } from '@equinor/fusion-framework-react';
 import { useContextResolver } from './useContextResolver';
+import type { NavigationModule } from '@equinor/fusion-framework-module-navigation';
+import type { AppModule, AppManifest } from '@equinor/fusion-framework-module-app';
+import { useObservableState } from '@equinor/fusion-observable/react';
+import { EMPTY, pairwise } from 'rxjs';
+import { ContextManifest } from '@equinor/fusion';
 
+type AppManifestWithContext = AppManifest & {context: ContextManifest | undefined}
 /**
  * See fusion-react-component storybook for available attributes
  * @link https://equinor.github.io/fusion-react-components/?path=/docs/data-contextselector--component
  * @returns JSX element
  */
 export const ContextSelector = (props: ContextSearchProps): JSX.Element | null => {
-  const { resolver, provider, currentContext } = useContextResolver();
-  
+  const { resolver, provider, currentContext: [selectedContextItem] } = useContextResolver(); 
+  const framework = useFramework<[AppModule, NavigationModule]>();
+  const {value: currentApp, complete} = useObservableState(useMemo(() => framework.modules.app.current$, [framework]));
+  const {value: appManifest} = useObservableState(useMemo(() => currentApp?.getManifest() ?? EMPTY, [currentApp]));
+
+  const {value: ctx} = useObservableState(useMemo(() => framework.modules.context.currentContext$.pipe(pairwise()), []));
+
   const updateContext = useCallback(
-    (e: ContextSelectEvent) => {
+    (e) => {
       if (provider) {
         if (e.type === 'select') {
-          if (e.nativeEvent.detail.selected.length) {
-            provider.contextClient.setCurrentContext(e.nativeEvent.detail.selected[0].id);
+          const ev = e as unknown as ContextSelectEvent;
+          if (ev.nativeEvent.detail.selected.length) {
+            provider.contextClient.setCurrentContext(ev.nativeEvent.detail.selected[0].id);
           }
         } else {
           provider.clearCurrentContext();
@@ -30,6 +42,19 @@ export const ContextSelector = (props: ContextSearchProps): JSX.Element | null =
     },
     [provider]
   );
+
+  const {navigator} = framework.modules.navigation;
+
+  useEffect(() => {
+    if(!ctx) return;
+    const [ previousContext, nextContext ] = ctx;
+    if(!nextContext) return;; // TODO
+    const manifest = appManifest as AppManifestWithContext;
+    const p = previousContext?.id ?? manifest?.context?.getContextFromUrl?.(navigator.location.pathname);
+    const url = p ? navigator.location.pathname.replace(p, nextContext?.id) : `/${nextContext?.id}`;
+    const reqId = requestAnimationFrame(() => navigator.replace(url));
+    return () => cancelAnimationFrame(reqId);
+  }, [ctx, navigator]);
 
   return (
     resolver && (
@@ -41,12 +66,12 @@ export const ContextSelector = (props: ContextSearchProps): JSX.Element | null =
             initialText={props.initialText ?? 'Start typing to search'}
             dropdownHeight={props.dropdownHeight ?? '300px'}
             variant={props.variant ?? 'header'}
-            autofocus={true}
-            previewItem={currentContext.length ? currentContext[0] : undefined}
-            value={currentContext.length ? currentContext[0].title : undefined}
-            selectedId={currentContext.length ? currentContext[0].id : undefined}
             onSelect={updateContext}
-            onClearContext={(e) => updateContext(e as unknown as ContextSelectEvent)}
+            autofocus={true}
+            previewItem={selectedContextItem}
+            value={selectedContextItem?.title}
+            selectedId={selectedContextItem?.title}
+            onClearContext={updateContext}
           />
         </ContextProvider>
       </div>
