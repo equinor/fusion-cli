6e81efde946d68a52a20d79e409bec0682419b36
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2023-07-27 19:47:42 +0200

    build: remove resolve from config
    webpack 5 works with default resolve.
    
    previous resolve used absolute paths which do not resolve sub node_modules
    
    [see](https://webpack.js.org/configuration/resolve/#resolvemodules)

--- src/build/parts/entry.ts
+++ src/build/parts/entry.ts
@@ -2,21 +2,7 @@ import * as path from 'path';
 import IPackage from '../Package';
 
 export default (modulePackage: IPackage, isProduction: boolean) => ({
-    entry: isProduction
-        ? path.resolve(process.cwd(), modulePackage.main)
-        : [path.resolve(process.cwd(), modulePackage.main), 'webpack-hot-middleware/client'],
-
-    resolve: {
-        extensions: ['.ts', '.js', '.tsx', '.jsx'],
-        modules: [
-            path.resolve(process.cwd(), 'node_modules'),
-            // mono-repo
-            path.resolve(process.cwd(), '..', '..', 'node_modules'),
-            path.dirname(path.resolve(process.cwd(), modulePackage.main)),
-            path.resolve(__dirname, '..', '..', '..', 'node_modules'),
-        ],
-        fallback: { 
-            path: false
-        }
-    },
+  entry: isProduction
+    ? path.resolve(process.cwd(), modulePackage.main)
+    : [path.resolve(process.cwd(), modulePackage.main), 'webpack-hot-middleware/client'],
 });
