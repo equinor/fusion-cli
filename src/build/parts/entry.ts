2b6ffe60c394b8d88a5d8e3834cc8b35c5c79283
Odin Thomas Rochmann <odin.rochmann@gmail.com>
2023-07-28 10:32:25 +0200

    build: update webpack plugin module resolver

--- src/build/parts/entry.ts
+++ src/build/parts/entry.ts
@@ -1,11 +1,20 @@
 import * as path from 'path';
 import IPackage from '../Package';
 
-export default (modulePackage: IPackage, isProduction: boolean) => ({
-  entry: isProduction
-    ? path.resolve(process.cwd(), modulePackage.main)
-    : [path.resolve(process.cwd(), modulePackage.main), 'webpack-hot-middleware/client'],
-  resolve: {
-    extensions: ['.ts', '.js', '.tsx', '.jsx'],
-  },
-});
+export default (modulePackage: IPackage, isProduction: boolean) => {
+  const cliNodeModulesPath = path.resolve(__dirname, '../../../node_modules');
+  const rootNodeModulesPath = path.resolve(cliNodeModulesPath, '../../..');
+  const modulePackageMain = path.resolve(process.cwd(), modulePackage.main);
+  return {
+    entry: isProduction
+      ? path.resolve(process.cwd(), modulePackage.main)
+      : [path.resolve(process.cwd(), modulePackage.main), 'webpack-hot-middleware/client'],
+    resolve: {
+      extensions: ['.ts', '.js', '.tsx', '.jsx'],
+      modules: [path.dirname(modulePackageMain), 'node_modules', rootNodeModulesPath],
+    },
+    resolveLoader: {
+      modules: ['node_modules', cliNodeModulesPath, rootNodeModulesPath],
+    },
+  };
+};
