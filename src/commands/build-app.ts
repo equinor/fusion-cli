66d08d789b0c140f843ea01c19c3de7c995108cb
Øyvind Eikeland <oyvind@eikeland.me>
2024-02-19 13:43:33 +0100

    fix(publish): fix type errors in prepack

--- src/commands/build-app.ts
+++ src/commands/build-app.ts
@@ -1,4 +1,5 @@
-import { Command, flags } from '@oclif/command';
+import { Command } from '@oclif/command';
+import { flags } from '@oclif/parser';
 import * as Listr from 'listr';
 import * as path from 'path';
 import * as fs from 'fs';
--- src/commands/start-app.ts
+++ src/commands/start-app.ts
@@ -1,16 +1,18 @@
 import { Command, flags } from '@oclif/command';
+// import { flags } from '@oclif/parser';
 
 import start from '../start-app/scripts/start';
 
 export default class StartApp extends Command {
   public static description = 'Start a fusion app';
 
-  public static flags = {
+  public static flags: any = {
     apps: flags.string({
       char: 'a',
       description: 'Compile one or more fusion apps. E.g. --apps AppKey1 AppKey2 AppKey3',
       multiple: true,
     }),
+    /* @ts-ignore */
     help: flags.help({ char: 'h' }),
     production: flags.boolean({ char: 'P', description: 'Use production config' }),
     port: flags.integer({ char: 'p', description: 'Devserver port' }),
@@ -19,6 +21,7 @@ export default class StartApp extends Command {
   public async run(): Promise<void> {
     console.log('Starting apps...');
     process.env.DEVELOPMENT = 'true';
+    /* @ts-ignore */
     const { port } = this.parse(StartApp).flags;
     start({ port });
   }
